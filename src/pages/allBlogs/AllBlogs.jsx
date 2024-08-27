90a048a398ad56d58463b8cbdea5528425038e23
aman78954 <125014489+aman78954@users.noreply.github.com>
2024-08-27 21:24:03 +0530

    Update AllBlogs.jsx

--- src/pages/allBlogs/AllBlogs.jsx
+++ src/pages/allBlogs/AllBlogs.jsx
@@ -1,103 +1,122 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import myContext from '../../context/data/myContext';
 import Layout from '../../components/layout/Layout';
 import { useNavigate } from 'react-router';
 
+// Utility function to remove HTML tags
+function stripHtmlTags(html) {
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+}
+
+// Utility function to truncate content to a specified word limit
+function truncateContent(html, wordLimit) {
+    const text = stripHtmlTags(html); // Remove HTML tags
+    const words = text.split(/\s+/); // Split by whitespace
+    if (words.length > wordLimit) {
+        return words.slice(0, wordLimit).join(' ') + '...'; // Return first 40 words with ellipsis
+    }
+    return text; // Return full text if less than 40 words
+}
+
 function AllBlogs() {
     const context = useContext(myContext);
     const { mode, getAllBlog } = context;
-
     const navigate = useNavigate();
-    function stripHtmlTags(html) {
-  const doc = new DOMParser().parseFromString(html, 'text/html');
-  return doc.body.textContent || "";
-}
+
     return (
         <Layout>
             <section className="text-gray-600 body-font">
-                <div className="container px-5 py-10 mx-auto max-w-7xl ">
-                    {/* Top Heading  */}
+                <div className="container px-5 py-10 mx-auto max-w-7xl">
+                    {/* Top Heading */}
                     <div className="mb-5">
-                        <h1 className=' text-center text-2xl font-bold'
-                            style={{ color: mode === 'dark' ? 'white' : 'black' }}>
+                        <h1 className='text-center text-2xl font-bold' style={{ color: mode === 'dark' ? 'white' : 'black' }}>
                             All Blogs
                         </h1>
                     </div>
-                    {/* Main Content  */}
+                    {/* Main Content */}
                     <div className="flex flex-wrap justify-center -m-4 mb-5">
-                        {/* Card 1  */}
-                        {getAllBlog.length > 0
-                            ?
-                            <>
-                                {getAllBlog.map((item, index) => {
-                                    const { thumbnail, id, date, content } = item
-                                    console.log(item)
-                                    return (
-                                        <div className="p-4 md:w-1/3" key={index}>
-                                            <div
-                                                style={{
-                                                    background: mode === 'dark'
-                                                        ? 'rgb(30, 41, 59)'
-                                                        : 'white',
-                                                    borderBottom: mode === 'dark'
-                                                        ?
-                                                        ' 4px solid rgb(226, 232, 240)'
-                                                        : ' 4px solid rgb(30, 41, 59)'
-                                                }}
-                                                className={`h-full shadow-lg  hover:-translate-y-1 cursor-pointer hover:shadow-gray-400
-                                                ${mode === 'dark'
-                                                    ? 'shadow-gray-700'
-                                                    : 'shadow-xl'
-                                                    } 
-                                                rounded-xl overflow-hidden`} 
-                                            >
-                                                {/* Blog Thumbnail  */}
-                                                <img onClick={() => navigate(`/bloginfo/${id}`)} className=" w-full" src={thumbnail} alt="blog" />
+                        {getAllBlog.length > 0 ? (
+                            getAllBlog.map((item, index) => {
+                                const { thumbnail, id, date, blogs } = item;
+                                const { title, content } = blogs;
+                                
+                                return (
+                                    <div className="p-4 md:w-1/3" key={index}>
+                                        <div
+                                            style={{
+                                                background: mode === 'dark' ? 'rgb(30, 41, 59)' : 'white',
+                                                borderBottom: mode === 'dark'
+                                                    ? '4px solid rgb(226, 232, 240)'
+                                                    : '4px solid rgb(30, 41, 59)'
+                                            }}
+                                            className={`h-full shadow-lg hover:-translate-y-1 cursor-pointer hover:shadow-gray-400
+                                                ${mode === 'dark' ? 'shadow-gray-700' : 'shadow-xl'}
+                                                rounded-xl overflow-hidden`}
+                                        >
+                                            {/* Blog Thumbnail */}
+                                            <img
+                                                onClick={() => navigate(`/bloginfo/${id}`)}
+                                                className="w-full"
+                                                src={thumbnail}
+                                                alt="blog thumbnail"
+                                            />
 
-                                                {/* Top Items  */}
-                                                <div className="p-6">
-                                                    {/* Blog Date  */}
-                                                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{
-                                                        color: mode === 'dark'
-                                                            ? 'rgb(226, 232, 240)'
-                                                            : ' rgb(30, 41, 59)'
-                                                    }}>
-                                                        {date}
-                                                    </h2>
+                                            {/* Top Items */}
+                                            <div className="p-6">
+                                                {/* Blog Date */}
+                                                <h2
+                                                    className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                                                    style={{
+                                                        color: mode === 'dark' ? 'rgb(226, 232, 240)' : 'rgb(30, 41, 59)'
+                                                    }}
+                                                >
+                                                    {date}
+                                                </h2>
 
-                                                    {/* Blog Title  */}
-                                                    <h1 className="title-font text-lg font-bold text-gray-900 mb-3" style={{
-                                                        color: mode === 'dark'
-                                                            ? 'rgb(226, 232, 240)'
-                                                            : ' rgb(30, 41, 59)'
-                                                    }}>
-                                                        {item.blogs.title}
-                                                    </h1>
+                                                {/* Blog Title */}
+                                                <h1
+                                                    className="title-font text-lg font-bold text-gray-900 mb-3"
+                                                    style={{
+                                                        color: mode === 'dark' ? 'rgb(226, 232, 240)' : 'rgb(30, 41, 59)'
+                                                    }}
+                                                >
+                                                    {title}
+                                                </h1>
 
-                                                    {/* Blog Description  */}
-                                                    <p className="leading-relaxed mb-3" style={{
-                                                        color: mode === 'dark'
-                                                            ? 'rgb(226, 232, 240)'
-                                                            : ' rgb(30, 41, 59)'
-                                                    }}>
-                                                        {stripHtmlTags(item.blogs.content)}
-                                                    </p>
-                                                </div>
+                                                {/* Blog Description */}
+                                                <p
+                                                    className="leading-relaxed mb-3"
+                                                    style={{
+                                                        color: mode === 'dark' ? 'rgb(226, 232, 240)' : 'rgb(30, 41, 59)'
+                                                    }}
+                                                >
+                                                    {truncateContent(content, 40)}
+                                                </p>
+
+                                                {/* See More Button */}
+                                                <button
+                                                    onClick={() => navigate(`/bloginfo/${item.id}`)}
+                                                    className="text-indigo-500 inline-flex items-center"
+                                                    style={{
+                                                        color: mode === 'dark' ? 'rgb(226, 232, 240)' : 'rgb(30, 41, 59)'
+                                                    }}
+                                                >
+                                                    See More
+                                                </button>
                                             </div>
                                         </div>
-                                    )
-                                })}
-                            </>
-                            :
-                            <>
-                                <h1 className='text-xl font-bold'>Not Found</h1>
-                            </>
-                        }
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <h1 className='text-xl font-bold'>Not Found</h1>
+                        )}
                     </div>
                 </div>
-            </section >
-        </Layout >
-    )
+            </section>
+        </Layout>
+    );
 }
 
-export default AllBlogs
+export default AllBlogs;
